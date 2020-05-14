28bd86269b868d1f2e6a7040e4030882686d1eef
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-14 22:35:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |word|
-    valid_calls.include? i
+    valid_calls.include? word
   end
 end
